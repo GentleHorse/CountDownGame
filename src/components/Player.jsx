536dea13596fee2c2a1553fd8babbf6c2116b0b6
GentleHorse <anch.abc.t@gmail.com>
2023-12-26 22:50:39 +0100

    Implement Tailwind CSS

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,23 +1,38 @@
 import { useState } from "react";
 
 export default function Player() {
-  const [enteredPlayerName, setEnteredPlayerName] = useState('');
+  const [enteredPlayerName, setEnteredPlayerName] = useState("");
   const [submitted, setSubmitted] = useState(false);
 
   const nameChangeHandler = (event) => {
     setEnteredPlayerName(event.target.value);
-  }
+  };
 
   const clickHandler = () => {
     setSubmitted(true);
-  }
+  };
+
+  const h2Classes = "text-teal-300 my-2.5 text-xl";
+  const inputClasses =
+    "border-solid border rounded-l border-lime-900 bg-emerald-900 p-1 text-teal-100";
+  const buttonClasses =
+    "cursor-pointer bg-lime-900 hover:bg-lime-700 border-solid border rounded-r border-lime-900 hover:border-lime-700 py-1.5 px-4";
 
   return (
-    <section id="player">
-      <h2>Welcome {submitted ? enteredPlayerName : 'unkown entity'}</h2>
-      <p>
-        <input type="text" onChange={nameChangeHandler} value={enteredPlayerName} />
-        <button onClick={clickHandler}>Set Name</button>
+    <section className="text-center">
+      <h2 className={h2Classes}>
+        Welcome {submitted ? enteredPlayerName : "unkown entity"}
+      </h2>
+      <p className="flex justify-center">
+        <input
+          className={inputClasses}
+          type="text"
+          onChange={nameChangeHandler}
+          value={enteredPlayerName}
+        />
+        <button className={buttonClasses} onClick={clickHandler}>
+          Set Name
+        </button>
       </p>
     </section>
   );
